59103741e39e2e2006e112178766b8ac361a144b
Joseph Tulungen <josephtulungen@gmail.com>
2025-11-24 13:06:38 +0800

    Menambahkan Rumus Fahrenheit

--- kalkulatorsuhu.py
+++ kalkulatorsuhu.py
@@ -1,1 +1,5 @@
+OFFSETFAHRENHEIT = 32
+RASIOFAHRENHEIT = 1.8
+
 celcius = int(input("Masukkan suhu (dalam bentuk celcius) disini... "))
+hasil_fahrenheit = (RASIOFAHRENHEIT * celcius) + OFFSETFAHRENHEIT
