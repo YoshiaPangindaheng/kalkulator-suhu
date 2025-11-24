abfe81102f942da3551cad7d6ae6c87ca6b9bd68
ZackTheSikma <zacklykimbal026@student.unsrat.ac.id>
2025-11-24 13:39:47 +0800

    Menambahkan print

--- kalkulatorsuhu.py
+++ kalkulatorsuhu.py
@@ -1,5 +1,7 @@
+# Program Perhitungan Suhu
 OFFSETFAHRENHEIT = 32
 RASIOFAHRENHEIT = 1.8
 
 celcius = int(input("Masukkan suhu (dalam bentuk celcius) disini... "))
 hasil_fahrenheit = (RASIOFAHRENHEIT * celcius) + OFFSETFAHRENHEIT
+print("Suhu dalam Fahrenheit adalah:", hasil_fahrenheit)
