7500b78e3b97f4387369430a57ccdc04291f0ca3
ZackTheSikma <zacklykimbal026@student.unsrat.ac.id>
2025-11-24 14:26:45 +0800

    Celcius ke Kelvin

--- kalkulatorsuhu.py
+++ kalkulatorsuhu.py
@@ -5,3 +5,8 @@ RASIOFAHRENHEIT = 1.8
 celcius = int(input("Masukkan suhu (dalam bentuk celcius) disini... "))
 hasil_fahrenheit = (RASIOFAHRENHEIT * celcius) + OFFSETFAHRENHEIT
 print("Suhu dalam Fahrenheit adalah:", hasil_fahrenheit)
+
+#Celcius To Kelvin
+OFFSETKELVIN = 273.15
+hasil_kelvin = celcius + OFFSETKELVIN
+print("Suhu dalam Kelvin adalah:", hasil_kelvin)
