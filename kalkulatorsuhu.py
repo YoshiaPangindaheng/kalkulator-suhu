5f48638f5a7fd6826bb7f77c39c2bd8c2406e16c
Farrel <yoshiapangindaheng026@student.unsrat.ac.id>
2025-11-25 08:49:40 +0800

    Menambahkan fitur celcius ke kelvin.

--- kalkulatorsuhu.py
+++ kalkulatorsuhu.py
@@ -1,12 +1,61 @@
-# Program Perhitungan Suhu
-OFFSETFAHRENHEIT = 32
-RASIOFAHRENHEIT = 1.8
+def celcius_fahrenheit(celcius):
+    """
+    Mengkalkulasikan hasil konversi suhu dari Celcius ke Fahrenheit
 
-celcius = int(input("Masukkan suhu (dalam bentuk celcius) disini... "))
-hasil_fahrenheit = (RASIOFAHRENHEIT * celcius) + OFFSETFAHRENHEIT
-print("Suhu dalam Fahrenheit adalah:", hasil_fahrenheit)
+    :param celcius: Nilai celcius yang akan dikonversikan
+    :return: Mengembalikan hasil konversi Celcius ke Fahrenheit
+    """
+    OFFSETFAHRENHEIT = 32
+    RASIOFAHRENHEIT = 1.8
+    fahrenheit = (RASIOFAHRENHEIT * celcius) + OFFSETFAHRENHEIT
+    return fahrenheit
 
-#Celcius To Kelvin
-OFFSETKELVIN = 273.15
-hasil_kelvin = celcius + OFFSETKELVIN
-print("Suhu dalam Kelvin adalah:", hasil_kelvin)
+def celcius_kelvin(celcius):
+    """
+    Mengkalkulasikan hasil konversi suhu dari Celcius ke Fahrenheit
+
+    :param celcius: Nilai celcius yang akan dikonversikan
+    :return: Mengembalikan hasil konversi Celcius ke Fahrenheit
+    """
+    OFFSETKELVIN = 273.15
+    kelvin = celcius + OFFSETKELVIN
+    return kelvin
+
+def cetak_hasil(konversi):
+    """
+    Memilih Jenis konversi kemudian mencetak hasil kalkulasi suhu
+
+    :param konversi: Menentukan Jenis Konversi
+    """
+    if konversi == 1:
+        celcius = int(input("Input Derajat Celcius: "))
+        print(celcius_fahrenheit(celcius))
+    elif konversi == 2:
+        celcius = int(input("Input Derajat Celcius: "))
+        print() # Masukkan Fungsi kalkulasi disini
+    elif konversi == 3:
+        fahrenheit = int(input("Input Derajat Fahrenheit: "))
+        print() # Masukkan Fungsi kalkulasi disini
+    elif konversi == 4:
+        fahrenheit = int(input("Input Derajat Fahrenheit: "))
+        print() # Masukkan Fungsi kalkulasi disini
+    elif konversi == 5:
+        kelvin = int(input("Input Derajat Kelvin: "))
+        print() # Masukkan Fungsi kalkulasi disini
+    elif konversi == 6:
+        kelvin = int(input("Input Derajat Kelvin: "))
+        print() # Masukkan Fungsi kalkulasi disini
+
+# Main Program
+print("Program Kalkulator Suhu")
+print("Pilih Jenis Konversi:")
+print(f"1 = C -> F \n"
+      f"2 = C -> K \n"
+      f"3 = F -> C \n"
+      f"4 = F -> K \n"
+      f"5 = K -> C \n"
+      f"6 = K -> F"
+      )
+
+pilihan = int(input())
+cetak_hasil(pilihan)
